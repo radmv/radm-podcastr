650e08cf9c6c554c60fb4f6d6c38c65618f391ec
Angga rizki <radmvarc@gmail.com>
2024-08-08 21:20:44 +0700

    left_sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,17 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <p className="text-white-1">Left Sidebar</p>
-      <main>{children}</main>
-      <p className="text-white-1">Right Sidebar</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        {children}
+        <p className="text-white-1">Right Sidebar</p>
+      </main>
     </div>
   );
 }
